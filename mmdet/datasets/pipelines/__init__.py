9c0abc8b4dd003a5b3d2c4718bbdb070cad09388
iserrano <iserrano@gradiant.org>
2022-04-27 16:09:49 +0200

    Remove multichannel normalization

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -13,7 +13,7 @@ from .transforms import (Albu, CutOut, Expand, MinIoURandomCrop, Normalize,
                          RandomCrop, RandomFlip, RandomShift, Resize,
                          SegRescale)
 
-from .manage_multichannel_image import (LoadMultiChannelImgFromFile, ResizeMultiChannel, NormalizeMultiChannel)
+from .manage_multichannel_image import (LoadMultiChannelImgFromFile, ResizeMultiChannel)
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
@@ -25,5 +25,5 @@ __all__ = [
     'InstaBoost', 'RandomCenterCropPad', 'AutoAugment', 'CutOut', 'Shear',
     'Rotate', 'ColorTransform', 'EqualizeTransform', 'BrightnessTransform',
     'ContrastTransform', 'Translate', 'RandomShift', 'LoadMultiChannelImgFromFile',
-    'ResizeMultiChannel', 'NormalizeMultiChannel'
+    'ResizeMultiChannel'
 ]
